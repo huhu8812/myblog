14fea7dcf6f1cac6737ebab95357218a260266f8
huhu8812 <252077095@qq.com>
2017-11-22 19:27:45 +0800

    no fab

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b7i0)nyf#$i06c9rrjlm&-s@9oj_ulq3mwnt=&6!z$aeiknvz4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.huhu8812.tech']
 
